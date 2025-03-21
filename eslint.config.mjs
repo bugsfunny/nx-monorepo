2efc646ec58feffe9e80c3e209c09fc965fc87ee
Jean-sebastien denize <js.denize@gmail.com>
2025-03-21 21:26:53 +0100

    initialize my-plugin and common-component libraries; remove unused files and configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,14 @@
 import nx from '@nx/eslint-plugin';
 
 export default [
+  {
+    files: ['**/*.json'],
+    // Override or add rules here
+    rules: {},
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
@@ -17,8 +25,33 @@ export default [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'domain:finance',
+              onlyDependOnLibsWithTags: ['domain:finance', 'domain:shared'],
+            },
+            {
+              sourceTag: 'domain:marketing',
+              onlyDependOnLibsWithTags: ['domain:marketing', 'domain:shared'],
+            },
+            {
+              sourceTag: 'domain:crm',
+              onlyDependOnLibsWithTags: ['domain:crm', 'domain:shared'],
+            },
+            {
+              sourceTag: 'domain:inventory',
+              onlyDependOnLibsWithTags: ['domain:inventory', 'domain:shared'],
+            },
+            {
+              sourceTag: 'domain:shared',
+              onlyDependOnLibsWithTags: ['domain:shared'],
+            },
+            {
+              sourceTag: 'type:app',
+              onlyDependOnLibsWithTags: [
+                'type:ui',
+                'type:util',
+                'type:feature',
+                'type:data-access',
+              ],
             },
           ],
         },
---
+++ tools/my-plugin/src/generators/angular-library-generator.spec.ts
@@ -0,0 +1,40 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nx/devkit';
+
+import { generateAngularLibraryGenerator } from './angular-library-generator';
+import { GenerateAngularLibraryGeneratorSchema } from './schema';
+
+describe('generate-angular-application generator', () => {
+  let tree: Tree;
+  const options: GenerateAngularLibraryGeneratorSchema = {
+    name: 'test',
+    domain: 'finance',
+    type: 'ui',
+  };
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generateAngularLibraryGenerator(tree, options);
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config).toBeDefined();
+  });
+
+  it('should ensure expected files exist after creation', async () => {
+    await generateAngularLibraryGenerator(tree, options);
+
+    const basePath = `libs/${options.domain}/${options.type}/${options.name}`;
+    const expectedFiles = [
+      `${basePath}/src/index.ts`,
+      `${basePath}/README.md`,
+      `${basePath}/project.json`,
+      `${basePath}/package.json`,
+    ];
+
+    expectedFiles.forEach((file) => {
+      expect(tree.exists(file)).toBe(true);
+    });
+  });
+});
